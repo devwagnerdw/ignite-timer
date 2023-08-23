b33addec4719b3ae28b7ea316eabbe4709783cb6
Vagne alves <vagnerdw1@gmail.com>
2023-08-23 19:36:58 -0300

    converting to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
-
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <Router />
-        <GlobalStyle />
       </BrowserRouter>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
